5813048fe6f4c9c3318ce60d0c7d80eb69141732
Nagaraju42 <nagarajukalngi@gmail.com>
2023-04-11 12:24:08 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from app.models import *
+
+# Create your views here.
+def display_dept(request):
+    LOD=DEPT.objects.all()
+    d={'dept':LOD}
+    return render(request,'dept.html',d)
+
+def display_emp(request):
+    LOE=EMP.objects.all()
+    d={'emp':LOE}
+    return render(request,'emp.html',d)
